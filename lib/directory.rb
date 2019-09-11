13beb1f06231c4b0bb69d45a0cd26df0dc38b8b2
Alexandria <alex.pugia816@hotmail.com>
2019-09-11 18:03:53 -0400

    Updated CLI in directory

--- lib/directory.rb
+++ lib/directory.rb
@@ -13,9 +13,9 @@ class CommandLineInterface
     end
 
     def user_prompt
-        prompt = TTY::Prompt.new
+        @prompt = TTY::Prompt.new
 
-        prompt.select("") do |menu|
+        @prompt.select("") do |menu|
             menu.choice 'New User', -> { new_user }
             menu.choice 'Returning Volunteer', -> { volunteer_log_in }
             menu.choice 'Returning Organization', -> { organization_log_in }
@@ -24,9 +24,7 @@ class CommandLineInterface
     end
 
     def new_user
-        prompt = TTY::Prompt.new
-
-        prompt.select("Please select an account option below:") do |menu|
+        @prompt.select("Please select an account option below:") do |menu|
             menu.choice "I'm a Volunteer", -> { new_volunteer }
             menu.choice "I'm an Organization", -> { new_organization }
             menu.choice 'Go back', -> { user_prompt }
@@ -34,9 +32,7 @@ class CommandLineInterface
     end
 
     def new_volunteer
-        prompt = TTY::Prompt.new
-
-        user = prompt.collect do
+        user = @prompt.collect do
             key(:first_name).ask('First Name:', required: true)
             key(:last_name).ask('Last Name:', required: true)
             puts ""
@@ -55,9 +51,7 @@ class CommandLineInterface
     end
 
     def new_organization
-        prompt = TTY::Prompt.new
-
-        user = prompt.collect do
+        user = @prompt.collect do
             key(:name).ask('Organization Name:', required: true)
             key(:city).ask('City:', required: true)
             key(:state).ask('State:', required: true)
@@ -77,10 +71,9 @@ class CommandLineInterface
     end
 
     def volunteer_log_in
-        prompt = TTY::Prompt.new
         first_name = "PLACEHOLDER"
 
-        user = prompt.collect do
+        user = @prompt.collect do
             first_name = "PLACEHOLDER"
 
             key(:username).ask('Enter your Username:', required: true)
@@ -96,10 +89,9 @@ class CommandLineInterface
     end
 
     def organization_log_in
-        prompt = TTY::Prompt.new
         name = "PLACEHOLDER"
 
-        user = prompt.collect do
+        user = @prompt.collect do
             first_name = "PLACEHOLDER"
 
             key(:username).ask('Enter your Username:', required: true)
@@ -115,76 +107,133 @@ class CommandLineInterface
     end
 
     def volunteer_main_menu
-        prompt = TTY::Prompt.new
-
         puts "*** MAIN MENU ***"
 
-        user = prompt.select("") do |menu|
+        user = @prompt.select("") do |menu|
             menu.choice "I'm here to Volunteer", -> { go_to_volunteer }
-            menu.choice 'View My Reviews', -> { my_reviews }
-            menu.choice 'View Previous Volunteer Records', -> { volunteer_records }
+            menu.choice 'View My Reviews', -> { my_reviews_volunteer }
+            menu.choice 'View Previous Volunteer Records', -> { volunteer_records_for_volunteer }
             menu.choice 'Update Profile', -> { volunteer_update_profile }
             menu.choice 'Log Out', -> { log_out }
         end
     end
 
     def organization_main_menu
-        prompt = TTY::Prompt.new
-
         puts "*** MAIN MENU ***"
 
-        user = prompt.select("") do |menu|
-            menu.choice 'View Previous Volunteer Records', -> { volunteer_records }
-            menu.choice 'View My Reviews', -> { my_reviews }
+        user = @prompt.select("") do |menu|
+            menu.choice 'View Previous Volunteer Records', -> { volunteer_records_for_organization }
+            menu.choice 'View My Reviews', -> { my_reviews_organization }
             menu.choice 'Update Profile', -> { organization_update_profile }
             menu.choice 'Log Out', -> { log_out }
         end
     end
 
-    def go_to_volunteer #for volunteers only
-        prompt = TTY::Prompt.new
+    def all_cities
+        Organization.all.map { |o| o.city }.uniq
+    end
 
+    def all_states
+        Organization.all.map { |o| o.state }.uniq
+    end
+
+    def go_to_volunteer
         puts "*** ORGANIZATIONS ***"
         puts ""
         puts "Please enter a city and state below to find organizations near you!"
-        input = prompt.collect do
+
+        input = @prompt.collect do
             key(:city).ask('City:', required: true)
             key(:state).ask('State:', required: true)
         end
-        
-       puts Organization.all.select { |o| o.city.downcase == input[:city].downcase && o.state.downcase == input[:state].downcase }
-        ### takes a (city, state) and outputs set number of organizations in that city, state
 
-        prompt.select("") { |m| m.choice "Exit", -> { volunteer_main_menu }}
+        orgs = Organization.all.select { |o| o.city.downcase == input[:city].downcase && o.state.downcase == input[:state].downcase }
+        org_names = orgs.map { |o| o.name }
+        puts ""
+        puts ""
+
+        selection = @prompt.select("Select an organization", org_names)
+        select_organization(selection)
+        puts ""
+        puts ""
+        puts "***************"
+        puts ""
     end
 
-    def my_reviews
-        prompt = TTY::Prompt.new
+    def select_organization(selection)
+        my_org = Organization.all.select { |o| o.name.downcase == selection.downcase }
 
+        puts "*** Welcome to #{selection}! ***"
+        puts ""
+        @prompt.select("") do |m| 
+            m.choice "Volunteer Now", -> { clock_in_and_out }
+            m.choice "Main Menu", -> { volunteer_main_menu }
+        end
+        puts ""
+    end
+
+    def clock_in_and_out
+        status = @prompt.select("") do |menu|
+            menu.choice 'Clock In', -> { clock_in }
+            menu.choice 'Clock Out', -> { clock_out}
+            menu.choice 'Main Menu', -> { volunteer_main_menu }
+        end
+    end
+
+    def clock_in
+        puts ""
+        puts "*"
+        puts "clocked in"
+        @prompt.select("") { |m| m.choice "Done", -> { volunteer_main_menu }}
+    end
+
+    def clock_out
+        puts ""
+        puts "*"
+        puts "clocked out"
+        @prompt.select("") { |m| m.choice "Done", -> { volunteer_main_menu }}
+    end
+
+    def my_reviews_volunteer
         puts "*** MY REVIEWS ***"
         puts ""
 
-        ### outputs the reviews a volunteer has left OR the reviews an organization has recieved
+        ### outputs the reviews a volunteer has left
 
-        prompt.select("") { |m| m.choice "Exit", -> { main_menu }}
+        @prompt.select("") { |m| m.choice "Exit", -> { volunteer_main_menu }}
     end
 
-    def volunteer_records
-        prompt = TTY::Prompt.new
+    def my_reviews_organization
+        puts "*** MY REVIEWS ***"
+        puts ""
 
+        ### outputs the reviews an organization has recieved
+
+        @prompt.select("") { |m| m.choice "Exit", -> { organization_main_menu }}
+    end
+
+    def volunteer_records_for_volunteer
         puts "*** VOLUNTEER RECORDS ***"
         puts ""
 
-        ### outputs all records for volunteers OR all volunteers that have volunteered at organization
+        ### outputs all records for volunteers
 
-        prompt.select("") { |m| m.choice "Exit", -> { main_menu }}
+        @prompt.select("") { |m| m.choice "Exit", -> { volunteer_main_menu }}
+    end
+
+    def volunteer_records_for_organization
+        puts "*** VOLUNTEER RECORDS ***"
+        puts ""
+
+        ### outputs all volunteers that have volunteered at organization
+
+        @prompt.select("") { |m| m.choice "Exit", -> { organization_main_menu }}
     end
 
     def volunteer_update_profile
-        prompt = TTY::Prompt.new
         puts "*** UPDATE PROFILE ***"
 
-        prompt.select("") do |menu| 
+        @prompt.select("") do |menu| 
             menu.choice "Update First Name", -> { } #update first name
             menu.choice "Update Last Name", -> { } #update last name 
             menu.choice "Update Password", -> { } #update password
@@ -194,10 +243,9 @@ class CommandLineInterface
     end
 
     def organization_update_profile
-        prompt = TTY::Prompt.new
         puts "*** UPDATE PROFILE ***"
 
-        prompt.select("") do |menu| 
+        @prompt.select("") do |menu| 
             menu.choice "Update Organization Name", -> { } #update name
             menu.choice "Update City", -> { } #update city
             menu.choice "Update State", -> { } #update state
